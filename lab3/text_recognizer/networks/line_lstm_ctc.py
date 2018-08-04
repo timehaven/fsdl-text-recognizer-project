c2e5dbebc2e52c72d5bf762aa4adf05d314e0a3c
timehaven <timehaven@local>
2018-08-04 06:35:21 +0000

    Use 16 epochs, stacked lstm for stolen initial lab3 soln

--- lab3/text_recognizer/networks/line_lstm_ctc.py
+++ lab3/text_recognizer/networks/line_lstm_ctc.py
@@ -52,8 +52,15 @@ def line_lstm_ctc(input_shape, output_shape, window_width=28, window_stride=14):
     convnet_outputs = TimeDistributed(convnet)(image_patches)
     # (num_windows, 128)
 
-    lstm_output = lstm_fn(128, return_sequences=True)(convnet_outputs)
-    # (num_windows, 128)
+    if 1:
+        lstm_output0 = lstm_fn(128, return_sequences=True)(convnet_outputs)
+        lstm_output = lstm_fn(128, return_sequences=True)(lstm_output0)
+        # (num_windows, 128)        
+    
+    else:
+        # Standard, submitted.
+        lstm_output = lstm_fn(128, return_sequences=True)(convnet_outputs)
+        # (num_windows, 128)        
 
     softmax_output = Dense(num_classes, activation='softmax', name='softmax_output')(lstm_output)
     # (num_windows, num_classes)
